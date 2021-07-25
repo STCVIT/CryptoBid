a4031b3c606e23a1c27b5935820e4f7342f06b59
Prerit2002 <Prerit715@gmail.com>
2021-07-25 23:25:03 +0530

    rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -9,21 +9,22 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic:'afraid voyage coyote stumble can air language express shine sign route medal',
+          providerOrUrl:
+            'https://rinkeby.infura.io/v3/3ab2896ae11d403786349d8911bd3f58'
+        }),
+      network_id: 4, // Ropsten's id
+      gas: 6700000, // Ropsten has a lower block limit than mainnet
+      confirmations: 0, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+      from:"0xF98974bBAA17122bb42f2EbA022c46875D68B3D0"
+    },
   },
-  rinkeby: {
-    provider: () =>
-      new HDWalletProvider({
-        mnemonic:'afraid voyage coyote stumble can air language express shine sign route medal',
-        providerOrUrl:
-          'https://rinkeby.infura.io/v3/6b7abd10acac4b48b9f24de881a9e6c8'
-      }),
-    network_id: 4, // Ropsten's id
-    gas: 5500000, // Ropsten has a lower block limit than mainnet
-    confirmations: 0, // # of confs to wait between deployments. (default: 0)
-    timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-    skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-    from:"0xF98974bBAA17122bb42f2EbA022c46875D68B3D0"
-  },
+  
 
   mocha: {
 
