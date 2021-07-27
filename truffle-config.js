36c3402f28a424521b34399263e7f497b71ec85f
Prerit2002 <Prerit715@gmail.com>
2021-07-27 22:42:01 +0530

    changes

--- truffle-config.js
+++ truffle-config.js
@@ -21,7 +21,7 @@ module.exports = {
       confirmations: 0, // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-      from:"0xF98974bBAA17122bb42f2EbA022c46875D68B3D0"
+      from:"0x5d21CAD6B77E6ba45aE4123BA5BE753D0e710FE6"
     },
   },
   
