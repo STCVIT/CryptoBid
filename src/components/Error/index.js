06dbd60fc59d8597517311fdc539e2e9abfa833b
Prerit2002 <Prerit715@gmail.com>
2021-08-02 20:49:38 +0530

    final changes

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -8,19 +8,23 @@ function Error(props){
     const Err = {
         1 : {
             Name : "Page Not Found" ,
-            img : i404
+            img : i404,
+            Link: '/'
         },
         2 : {
             Name : "Not Signed in" ,
-            img: Login
+            img: Login,
+            Link: '/profile'
         },
         3 : {
             Name : "Account not connected" ,
-            img: Account
+            img: Account,
+            Link: '/'
         },
         4 : {
             Name : "Metamask not installed" ,
-            img : Metamask
+            img : Metamask,
+            Link: 'https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en'
         }
 
 };
@@ -28,7 +32,7 @@ console.log(Err);
     return(
         <div className={styles.Cont + " text-center container my-3 "}>
             <h3>{Err[props.id].Name}</h3>
-            <p>Head back to the homepage <a href="/">CryptoAuction</a></p>
+            <p>Head back to the homepage <a href={Err[props.id].Link} >CryptoAuction</a></p>
             <img src={Err[props.id].img } className={styles.e404} width="50%" alt="error" />
             </div>
          
