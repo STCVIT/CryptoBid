59a4277de11fb9ea193c638a4d3b5e64d27b2708
Prerit2002 <Prerit715@gmail.com>
2022-04-06 22:35:09 +0530

    changes

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -2,18 +2,33 @@ import React from "react";
 import styles from "./postCards.module.css";
 import { AiFillInfoCircle, AiFillThunderbolt } from "react-icons/ai";
 function Card(props) {
+  
   return (
     <>
       <a className={styles.LinkTag} href={"/product/" + props.Id}>
       <div className={styles.card + " card"}>
         <div>
-          <img
+          { props.img === "https://ipfs.infura.io/ipfs/" ? 
+           <img
+           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
+           alt="Product-Img"
+           className={"card-img-top " + styles.imgWatch}
+           width="250px"
+           height="250px"
+         /> : 
+
+         <img
             src={props.img}
             alt="Product-Img"
             className={"card-img-top " + styles.imgWatch}
             width="250px"
             height="250px"
           />
+
+          
+        
+        }
+          
         </div>
         <div>
           <h6 className={"heading " + styles.heading}>{props.Name}</h6>
